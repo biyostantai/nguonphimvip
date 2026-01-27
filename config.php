c4411bbf3d71d882719f10fe89e509b4d63cee97
Le Tan Tai <letan@taidethuong.com>
2026-01-27 13:38:15 +0700

    Switch to static sitemap.xml, remove PHP files

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'nguonphimvip';
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-if ($conn->connect_error) { die("Kết nối thất bại: " . $conn->connect_error); }
-$conn->set_charset("utf8");
