54a53294127c14e6f35e64888335de7c7fdd2404
Le Tan Tai <letan@taidethuong.com>
2026-01-27 13:14:54 +0700

    Fix: Remove all whitespace from config.php

--- config.php
+++ config.php
@@ -1,18 +1,8 @@
-<?php 
-// config.php 
-// Đảm bảo không có dòng trống nào phía trên thẻ <?php này 
-
-$db_host = 'localhost'; 
-$db_user = 'root';  
-$db_pass = '';      
-$db_name = 'nguonphimvip';  
-
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name); 
-
-if ($conn->connect_error) { 
-    die("Kết nối thất bại: " . $conn->connect_error); 
-} 
-
-$conn->set_charset("utf8"); 
-
-// Tuyệt đối KHÔNG thêm thẻ đóng ?> ở đây để tránh chèn dòng trống vô ý 
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'nguonphimvip';
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error) { die("Kết nối thất bại: " . $conn->connect_error); }
+$conn->set_charset("utf8");
